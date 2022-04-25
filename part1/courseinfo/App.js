02f158cf328e02204c1ba9dfc0a35552e1b9410b
Woo Zway <wuzw@zju.edu.cn>
2022-04-25 15:53:58 +0800

    -

--- part1/courseinfo/App.js
+++ part1/courseinfo/App.js
@@ -15,7 +15,7 @@ const Content = (props) => {
     <div>
       <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
       <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[2].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   )
 }
