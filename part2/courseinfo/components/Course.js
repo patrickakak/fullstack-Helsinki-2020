1c0ae7baf344a0d3ba0f45c14d8f65ef1b8fbc01
Woo Zway <wuzw@zju.edu.cn>
2022-04-25 17:48:26 +0800

    -

--- part2/courseinfo/components/Course.js
+++ part2/courseinfo/components/Course.js
@@ -1,26 +1,13 @@
-import React from "react"
+const Header = ({ course }) => <h1>{course}</h1>
 
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
-}
+const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
 
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part, i) => (
-        <Part key={i} name={part.name} exercises={part.exercises}></Part>
-      ))}
-    </div>
-  )
-}
+const Content = ({ parts }) =>
+  <>
+    {parts.map((part, i) => (
+      <Part key={i} name={part.name} exercises={part.exercises}></Part>
+    ))}
+  </>
 
 const Total = ({ parts }) => {
   const total = parts.reduce((prev, cur) => prev + cur.exercises, 0)
@@ -31,15 +18,13 @@ const Total = ({ parts }) => {
 const Course = ({ courses }) => {
   return (
     <>
-      {courses.map((course, i) => {
-        return (
-          <div key={i}>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-          </div>
-        )
-      })}
+      {courses.map((course, index) =>
+        <div key={index}>
+          <Header course={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      )}
     </>
   )
 }
