1b2f0a129f88ac022dd21ff14d0b3826852d9c72
wuziwei <wuziwei@echaincity.com>
2022-04-26 15:09:27 +0800

    restructure dir

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { Fragment, useState } from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const Filter = ({ handleFilterChange }) => {
   return (
@@ -41,19 +42,22 @@ const Persons = ({ personsToShow }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   const addPerson = (event) => {
     event.preventDefault()
-    const found = persons.find(person => person.name == newName)
+    const found = persons.find(person => JSON.stringify(person.name) === JSON.stringify(newName))
     if (found) {
       alert(`${newName} is already added to phonebook`)
       return
@@ -83,7 +87,7 @@ const App = () => {
   const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filterText))
 
   return (
-    <Fragment>
+    <>
       <h2>Phonebook</h2>
       <Filter handleFilterChange={handleFilterChange} />
       <h3>add a new</h3>
@@ -96,7 +100,7 @@ const App = () => {
       />
       <h3>Numbers</h3>
       <Persons personsToShow={personsToShow} />
-    </Fragment>
+    </>
   )
 }
 
