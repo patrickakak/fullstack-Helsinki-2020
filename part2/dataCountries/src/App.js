ebcbd09a464abda4465b99c291ebba9d4e25915b
woozway <22060468@zju.edu.cn>
2022-04-27 20:29:38 +0800

    finish data for countries (ex2.11.-2.14)

--- part2/dataCountries/src/App.js
+++ part2/dataCountries/src/App.js
@@ -1,40 +1,11 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-const SubView = ({ country }) => {
-  return (
-    <div>
-      <h2>{country.name.common}</h2>
-      <div>capital {country.capital}</div>
-      <div>area {country.area}</div>
-      <h3>languages</h3>
-      <ul>
-        {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
-      </ul>
-      <img src={country.flags['png']} alt="" />
-    </div>
-  )
-}
-
-const WeatherView = ({ country, weatherData }) => {
-  const weatherIconURL = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
-
-  return (
-    <div>
-      <h3>Weather in {country.capital}</h3>
-      <div>temperature {(weatherData.main.temp - 273.15).toFixed(2)} Celcius</div>
-      <img src={weatherIconURL} alt="" />
-      <div>wind {weatherData.wind.speed} m/s</div>
-    </div>
-  )
-}
-
 const View = ({ country }) => {
   const [isLoading, setLoading] = useState(true)
   const [weatherData, setWeatherData] = useState()
 
-  const API_KEY = process.env.REACT_APP_API_KEY
-  const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${API_KEY}`
+  const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${process.env.REACT_APP_API_KEY}`
   useEffect(() => {
     axios
       .get(API_URL)
@@ -45,15 +16,24 @@ const View = ({ country }) => {
   }, [API_URL])
 
   if (isLoading) {
-    return (
-      <SubView country={country} />
-    )
+    return <></>
   }
+  const weatherIconURL = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
   return (
-    <>
-      <SubView country={country} />
-      <WeatherView country={country} weatherData={weatherData} />
-    </>
+    <div>
+      <h2>{country.name.common}</h2>
+      <div>capital {country.capital}</div>
+      <div>area {country.area}</div>
+      <h3>languages</h3>
+      <ul>
+        {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
+      </ul>
+      <img src={country.flags['png']} alt="" />
+      <h3>Weather in {country.capital}</h3>
+      <div>temperature {(weatherData.main.temp - 273.15).toFixed(2)} Celcius</div>
+      <img src={weatherIconURL} alt="" />
+      <div>wind {weatherData.wind.speed} m/s</div>
+    </div>
   )
 }
 
