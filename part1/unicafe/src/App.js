192a15868ad816e2c264dc85583ddebe967e3bdd
woozway <22060468@zju.edu.cn>
2022-05-08 10:26:54 +0800

    modify unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,48 +4,40 @@ const Display = ({ text }) => <h1>{text}</h1>
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
-const StatisticLine = ({ text, value }) => <tr><td>{text}</td><td>{value}</td></tr>
+const Statistic = ({ text, value }) => <tr><td>{text}</td><td>{value}</td></tr>
 
+// eslint-disable-next-line
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
   const average = (good * 1 + neutral * 0 + bad * -1) / all
-  const positive = good / all
+  const positive = good / all * 100 + "%"
 
   if (all === 0) {
-    return (
-      <div>
-        No feedback given
-      </div>
-    )
+    return <p>No feedback given</p>
   }
   return (
     <table>
       <tbody>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive * 100 + "%"} />
+        <Statistic text="good" value={good} />
+        <Statistic text="neutral" value={neutral} />
+        <Statistic text="bad" value={bad} />
+        <Statistic text="all" value={all} />
+        <Statistic text="average" value={average} />
+        <Statistic text="positive" value={positive} />
       </tbody>
     </table>
   )
 }
 
 const App = () => {
+  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const incrementGood = () => {
-    setGood(good + 1)
-  }
-  const incrementNeutral = () => {
-    setNeutral(neutral + 1)
-  }
-  const incrementBad = () => {
-    setBad(bad + 1)
-  }
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad = () => setBad(bad + 1)
 
   return (
     <div>
