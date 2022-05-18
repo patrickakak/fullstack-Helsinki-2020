87e0ae726322d02cfd9b6f478dea74b6b8d7a8e5
woozway <22060468@zju.edu.cn>
2022-05-18 23:44:59 +0800

    finish ex5.1.-5.4

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -7,11 +7,16 @@ import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [successMessage, setSuccessMessage] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -20,6 +25,9 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
@@ -32,31 +40,90 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedInBloglistUser')
+    setUser(null)
+  }
+
+  const handleCreate = async () => {
+    const newObject = {
+      title: title,
+      author: author,
+      url: url,
+    }
+    await blogService.create(newObject)
+    const updatedBlogs = await blogService.getAll()
+    setBlogs(updatedBlogs)
+    setSuccessMessage(`A new blog ${newObject.title} by ${newObject.author} added`)
+    setTimeout(() => {
+      setSuccessMessage(null)
+    }, 5000)
+  }
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   return (
     <div>
-      <h2>Log in to application</h2>
+      {user === null ? <h2>Log in to application</h2> : <h2>blogs</h2>}
+      <Notification message={successMessage} type='success' />
       <Notification message={errorMessage} type='error' />
-      {user === null ?
+      {user === null &&
         <LoginForm
           handleLogin={handleLogin}
           username={username}
           password={password}
           setUsername={setUsername}
           setPassword={setPassword}
-        /> :
-        <div>
-          <p>{user.name} logged-in</p>
-        </div>
+        />
       }
       {user !== null &&
         <div>
-          <h2>blogs</h2>
+          <div>
+            {user.username} logged in
+            <button onClick={handleLogout}>logout</button>
+          </div>
+          <h2>create new</h2>
+          <div>
+            title:
+            <input
+              type='text'
+              value={title}
+              name='Title'
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </div>
+          <div>
+            author:
+            <input
+              type='text'
+              value={author}
+              name='Author'
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </div>
+          <div>
+            url:
+            <input
+              type='text'
+              value={url}
+              name='Url'
+              onChange={({ target }) => setUrl(target.value)}
+            />
+          </div>
+          <button onClick={handleCreate}>create</button>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
