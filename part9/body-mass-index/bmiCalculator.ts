863150dcf3d24d79c67e9248c2425a64c1bbef8f
woozway <22060468@zju.edu.cn>
2022-06-10 15:29:05 +0800

    finish ex9.1.-ex9.3.

--- part9/body-mass-index/bmiCalculator.ts
+++ part9/body-mass-index/bmiCalculator.ts
@@ -1,4 +1,23 @@
-export const calculateBmi = (height: number, weight: number): string => {
+interface InputValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: Array<string>): InputValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / ((height / 100) * (height / 100));
 
   if (bmi < 15) {
@@ -21,4 +40,13 @@ export const calculateBmi = (height: number, weight: number): string => {
   return `Oops something went wrong, unable to calculate bmi`;
 };
 
-console.log(calculateBmi(180, 74))
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
