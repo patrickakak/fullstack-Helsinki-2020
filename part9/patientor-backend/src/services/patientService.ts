088f68824a1defd4856b146a34ce3d0ebb9643d3
woozway <22060468@zju.edu.cn>
2022-06-11 16:12:15 +0800

    finish ex9.8.-ex9.9.

---
+++ part9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,23 @@
+import patientsData from "../../data/patients.json";
+import { NonSensitivePatient, Patient } from "../types";
+
+const patients: Array<Patient> = patientsData;
+
+const getPatients = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitivePatients = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default {
+  getPatients,
+  getNonSensitivePatients,
+};
